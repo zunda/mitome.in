c5d8276d03c9d196149f52553f8ef58f1ea1f8f1
zunda <zundan@gmail.com>
2020-06-11 22:25:29 -1000

    Try hosting Vue components

---
+++ docs/.vuepress/components/RsaKey.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h3>公開鍵</h3><pre>{{ privateKey }}</pre>
+    <h3>私有鍵</h3><pre>{{ publicKey }}</pre>
+    <button v-on:click="generateKey">鍵対を生成する</button>
+    <button v-on:click="clearKey">鍵対を消す</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      privateKey: "",
+      publicKey: ""
+    }
+  },
+  methods: {
+    generateKey: function () {
+      this.privateKey = "Private Key"
+      this.publicKey = "Public Key"
+    },
+    clearKey: function () {
+      this.privateKey = ""
+      this.publicKey = ""
+    }
+  }
+}
+</script>
+
+<style>
+pre {
+  background-color: #282c34;
+  color: #a0f0a0;
+}
+</style>
