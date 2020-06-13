5ae09811ef0c48fff030c1184f54e15e14b8a790
zunda <zundan@gmail.com>
2020-06-12 23:10:25 -1000

    Improve Navbar

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'mitome.in',
+  description: '認印の代わりとしてOpenPGPの利用方法を考えます。',
+  themeConfig: {
+    nav: [
+      { text: 'ホーム', link: '/' }
+    ],
+    repo: 'zunda/mitome.in'
+  }
+}
