afc243429f016d5ed1855d2b69629eb4e07d8e41
zunda <zundan@gmail.com>
2020-06-14 22:12:25 -1000

    Format timestamp better

--- docs/.vuepress/components/ReadKey.vue
+++ docs/.vuepress/components/ReadKey.vue
@@ -9,7 +9,7 @@
       <li>鍵の種類: {{ klass }}</li>
       <li>名前: {{ name }}</li>
       <li>電子メールアドレス: {{ email }}</li>
-      <li>生成時刻:<br>{{ created }}</li>
+      <li>生成時刻: {{ created }}</li>
       <li>指紋(fingerprint):<br>{{ fingerprint }}</li>
     </ul>
   </div>
@@ -18,9 +18,11 @@
 <script>
 import Vue from 'vue'
 
-import VueToast from 'vue-toast-notification';
-import 'vue-toast-notification/dist/theme-default.css';
-Vue.use(VueToast);
+import VueToast from 'vue-toast-notification'
+import 'vue-toast-notification/dist/theme-default.css'
+Vue.use(VueToast)
+
+import moment from 'moment'
 
 const OpenPgp = require('openpgp')
 
@@ -48,7 +50,7 @@ export default {
         this.name = key.keys[0].users[0].userId.name
         this.email = key.keys[0].users[0].userId.email
         this.klass = key.keys[0].keyPacket.constructor.name
-        this.created = key.keys[0].keyPacket.created
+        this.created = moment(key.keys[0].keyPacket.created).format('YYYY年MM月DD日 HH:MM:SS Z')
         this.fingerprint =
           Array.from(key.keys[0].keyPacket.fingerprint)
           .map(x => ('0' + x.toString(16).toUpperCase()).slice(-2)).join(' ')
