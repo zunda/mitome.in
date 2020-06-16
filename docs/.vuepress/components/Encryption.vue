a41ae843dc79200814ef4ec485588f5153fe8ea7
zunda <zundan@gmail.com>
2020-06-15 22:37:50 -1000

    Make it possible to add/remove public keys for repients

--- docs/.vuepress/components/Encryption.vue
+++ docs/.vuepress/components/Encryption.vue
@@ -8,7 +8,14 @@
       <br>
       <textarea v-model="newPubKey" class="key" spellcheck="false" />
     </p>
-    <p>ここに公開鍵のリストが表示される</p>
+    <ul id="public-keys">
+      <li v-for="pubKey in pubKeys" :key="pubKey.keyId">
+        <button v-on:click="removePubKey(pubKey.keyId)" title="鍵をリストから取り除く"><Fa-Remove /></button>
+        <span class="key-id">{{ pubKey.keyId }}</span>:
+        {{ pubKey.name }}
+        <span class="email">&lt;{{ pubKey.emali }}&gt;</span>
+      </li>
+    </ul>
     <p>
       <button v-bind:disabled="processing" v-on:click="encrypt">
         暗号化する
@@ -48,6 +55,37 @@ export default {
   },
   methods: {
     addPubKey: function () {
+      this.processing = true
+      OpenPgp.key.readArmored(this.newPubKey)
+      .then((key) => {
+        if (key.keys.length < 1) {
+          throw {message: '有効な鍵が見つかりませんでした'}
+        }
+        const newKey = key.keys[0]
+        if (newKey.keyPacket.tag != 6 && newKey.keyPacket.tag != 14) {
+          throw {message: '公開鍵ではありません'}
+        }
+        const newKeyId = newKey.keyPacket.keyid.toHex()
+        if (this.pubKeys.find(key => key.keyId === newKeyId)) {
+          throw {message: '同じIDの鍵が登録されています'}
+        }
+        this.pubKeys.unshift({
+          keyId: newKeyId,
+          name: newKey.users[0].userId.name,
+          emali: newKey.users[0].userId.email,
+          key: newKey
+        })
+        this.newPubKey = ''
+      }).catch((e) => {
+        console.log(e)
+        Vue.$toast.open({message: e.message, type: 'error'})
+
+      }).finally(() => {
+        this.processing = false
+      })
+    },
+    removePubKey: function (keyId) {
+      this.pubKeys = this.pubKeys.filter(key => key.keyId !== keyId)
     },
     encrypt: function() {
     },
@@ -62,7 +100,5 @@ export default {
       })
     }
   },
-  mounted() {
-  }
 }
 </script>
