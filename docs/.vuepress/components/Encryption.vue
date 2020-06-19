60082a8082a278d44c38be60f2e566f5e2204b76
zunda <zundan@gmail.com>
2020-06-18 22:50:45 -1000

    Wrap textarea with InputArea

--- docs/.vuepress/components/Encryption.vue
+++ docs/.vuepress/components/Encryption.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <p>下記にペーストした公開鍵を受取人に
+    <p>下記の公開鍵を受取人に
       <button v-bind:disabled="processing" v-on:click="addPublicKey">
         追加する
       </button>
       <br>
-      <textarea v-model="newPublicKey" class="key" spellcheck="false" placeholder="受取人の公開鍵" />
-      <button v-bind::disabled="processing" v-on:click="clearNewPublicKey" title="入力中の公開鍵を消去する" style="float:right;">
+      <textarea
+        v-model="newPublicKey"
+        class="key"
+        spellcheck="false"
+        placeholder="追加する受取人の公開鍵"
+      />
+      <button
+        v-bind::disabled="processing"
+        v-on:click="clearNewPublicKey"
+        title="追加する受取人の公開鍵を消去する"
+        style="float:right;">
         <Fa-Eraser />
       </button>
     </p>
@@ -18,12 +27,17 @@
         <span class="email">&lt;{{ publicKey.emali }}&gt;</span>
       </li>
     </ul>
-    <p>メッセージ<br><textarea v-model="message" class="cleartext" placeholder="暗号化されるメッセージ" /></p>
     <p>
+      上記のリストの公開鍵に宛てて下記のメッセージを
       <button v-bind:disabled="processing" v-on:click="encrypt">
         暗号化する
       </button>
     </p>
+    <InputArea section="EncryptionMessage"
+      cssClass="cleartext"
+      name="暗号化するメッセージ"
+      v-bind:disabled="processing"
+    />
     <p>暗号文
       <button @click="copyEncryptedMessage" title="暗号文をクリップボードにコピーする">
         <Fa-Copy />
@@ -49,11 +63,14 @@ const OpenPgp = require('openpgp')
 export default {
   data() {
     return {
-      message: "",
       newPublicKey: "",
-      publicKeys: [],
       encryptedMessage: "",
       processing: false
+    }
+  },
+  computed: {
+    publicKeys: function() {
+      return this.$store.state.publicKeys || []
     }
   },
   methods: {
@@ -62,7 +79,11 @@ export default {
       OpenPgp.key.readArmored(this.newPublicKey)
       .then(data => {
         if (data.keys.length < 1) {
-          throw {message: '有効な鍵が見つかりませんでした'}
+          if (data.err.length > 0) {
+            throw data.err[0]
+          } else {
+            throw {message: '有効な鍵が見つかりませんでした'}
+          }
         }
         if (data.keys.length > 1) {
           Vue.$toast.open({message: '複数の鍵が見つかりました。最初の鍵だけ追加します', type: 'warning'})
@@ -97,11 +118,13 @@ export default {
       this.publicKeys = this.publicKeys.filter(key => key.keyId !== keyId)
       this.commitPublicKeys()
     },
+    commitPublicKeys: function() {
+      this.$store.commit('setPublicKeys', this.publicKeys)
+    },
     encrypt: function() {
       this.processing = true
-      this.encryptedMessage = ''
       OpenPgp.encrypt({
-        message: OpenPgp.message.fromText(this.message),
+        message: OpenPgp.message.fromText(this.$store.state.inputText.EncryptionMessage || ''),
         publicKeys: this.publicKeys.map(x => x.key)
       }).then(result => {
         this.encryptedMessage = result.data
@@ -112,9 +135,6 @@ export default {
         this.processing = false
       })
     },
-    commitPublicKeys: function() {
-      this.$store.commit('setPublicKeys', this.publicKeys)
-    },
     copyEncryptedMessage: function() {
       this.$copyText(this.encryptedMessage).then(() => {
         Vue.$toast.open({message: '暗号文をコピーしました', type: 'info'})
@@ -122,11 +142,6 @@ export default {
         console.log(e)
         Vue.$toast.open({message: e, type: 'error', duration: 60000})
       })
-    }
-  },
-  mounted() {
-    if (this.$store.state.publicKeys.length > 0) {
-      this.publicKeys = this.$store.state.publicKeys
     }
   }
 }
